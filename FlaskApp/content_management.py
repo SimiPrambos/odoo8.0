307861a2dfd001458aa90073e88e336f17c371fd
SimiPrambos <simi.prambos@gmail.com>
2017-04-07 10:01:12 +0700

    removed folder

--- FlaskApp/content_management.py
+++
@@ -1,3 +0,0 @@
-def Content():
-	DIC = {"pertama": [["Membuat Content Management", "/content-management-flask/"],["Making Content Management", "/make-content-management/"],["Membuat menu dan link", "membuat-menu-dan-link"]]}
-	return DIC
